bc46c5d98472a26a870684cb6d19221a6ce300a8
demonyangyue <demonyangyue@gmail.com>
2014-03-30 20:50:40 +0800

    fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'https://github.com/demonyangyue/depot.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/home/#{user}/programme/ruby/rails/#{application}"
+set :deploy_to, "/home/#{fetch(:user)}/programme/ruby/rails/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
